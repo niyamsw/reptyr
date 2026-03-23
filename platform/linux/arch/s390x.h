e042ee1a4f6d73be49120c85b00fe25acbd8be45
Niyam Siwach <niyam@ibm.com>
2026-03-23 14:59:05 +0530

    fixes

--- platform/linux/arch/s390x.h
+++ platform/linux/arch/s390x.h
@@ -1,7 +1,3 @@
-#ifndef NT_S390_SYSTEM_CALL
-#define NT_S390_SYSTEM_CALL 0x307
-#endif
-
 static struct ptrace_personality arch_personality[1] = {
     {
         offsetof(s390_regs, gprs[2]),
@@ -17,33 +13,21 @@ static struct ptrace_personality arch_personality[1] = {
 
 static inline void arch_fixup_regs(struct ptrace_child *child) {
     child->regs.psw.addr -= S390_SYSCALL_SIZE;
+    child->regs.gprs[2] = child->regs.orig_gpr2;
 }
 
 static inline int arch_set_syscall(struct ptrace_child *child,
                                    unsigned long sysno) {
-    unsigned int syscall_reg = sysno;
-    struct iovec reg_iovec = {
-        .iov_base = &syscall_reg,
-        .iov_len = sizeof(syscall_reg)
-    };
-    return ptrace_command(child, PTRACE_SETREGSET, NT_S390_SYSTEM_CALL,
-                          &reg_iovec);
+    return ptrace_command(child, PTRACE_POKEUSER, PT_GPR2, sysno);
 }
 
 static inline int arch_save_syscall(struct ptrace_child *child) {
-    unsigned int syscall_reg;
-    struct iovec reg_iovec = {
-        .iov_base = &syscall_reg,
-        .iov_len = sizeof(syscall_reg)
-    };
-    if (ptrace_command(child, PTRACE_GETREGSET, NT_S390_SYSTEM_CALL,
-                       &reg_iovec) < 0)
+    child->saved_syscall = ptrace_command(child, PTRACE_PEEKUSER, PT_GPR2);
+    if (child->error)
         return -1;
-
-    child->saved_syscall = syscall_reg;
     return 0;
 }
 
 static inline int arch_restore_syscall(struct ptrace_child *child) {
-    return arch_set_syscall(child, child->saved_syscall);
+    return 0;
 }
